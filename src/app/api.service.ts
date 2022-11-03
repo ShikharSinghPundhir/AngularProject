09bcec16ee64f766c5872fb9b5afa20b876c5562
ShikharSinghPundhir <shikharpundhir84@gmail.com>
2022-11-03 21:14:40 +0530

    d

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
   url = "https://jsonplaceholder.typicode.com/posts"
-  url1 = "https://newsapi.org/v2/everything?q=tesla&from=2022-08-13&sortBy=publishedAt&apiKey=ff481830b6114f9c8c367c17977e7767"
+  url1 = "https://newsapi.org/v2/everything?q=tesla&from=2022-09-09&sortBy=publishedAt&apiKey=ff481830b6114f9c8c367c17977e7767"
 
   constructor(private http: HttpClient) { }
   alldata() {
